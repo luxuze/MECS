a5c4979f5fa79e6f89f04cdaaa3a7870d7cd24a9
luxuze <luxuze@icloud.com>
2020-10-03 20:59:34 +0800

    uap

--- data/data.py
+++ data/data.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
 
 x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-y = [2.5, 1.4, 1.2, 0.7, 0.6, 0.5, 0.6, 0.7, 0.8, 1.2]
+y = [4.7, 1.7, 1.2, 0.9, 0.78, 0.81, 0.84, 0.92, 0.99, 1.1]
 
 
-plt.plot(x, y, color="r", linestyle="--", marker="*", linewidth=1.0)
+plt.plot(x, y, color="r", linestyle="-", marker=".", linewidth=1.0)
 
 plt.show()
