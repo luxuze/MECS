e48619b7a6c1521829267fa2d97bdbf050b7aaef
luxuze <luxuze@icloud.com>
2020-10-03 17:20:39 +0800

    slog recycle

---
+++ data/data.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+y = [2.5, 1.4, 1.2, 0.7, 0.6, 0.5, 0.6, 0.7, 0.8, 1.2]
+
+
+plt.plot(x, y, color="r", linestyle="--", marker="*", linewidth=1.0)
+
+plt.show()
